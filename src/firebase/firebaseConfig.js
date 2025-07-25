c3b7bdc352c5a776b95f86832a4aeaf7401ca5f0
Vighnesh B <vighnesh.b@hotmail.com>
2025-07-26 02:25:36 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgpwbafw3jgtsXDQ4WK3l8PIOAQ745Kvk",
-  authDomain: "nfts-bb29f.firebaseapp.com",
-  projectId: "nfts-bb29f",
-  storageBucket: "nfts-bb29f.appspot.com", // Note: should be .appspot.com, not .firebasestorage.app
-  messagingSenderId: "989047203997",
-  appId: "1:989047203997:web:99d6ae004ebf1a0c598c74",
-  measurementId: "G-75FFFR8E6V"
+  
 };
 
 // Initialize Firebase
